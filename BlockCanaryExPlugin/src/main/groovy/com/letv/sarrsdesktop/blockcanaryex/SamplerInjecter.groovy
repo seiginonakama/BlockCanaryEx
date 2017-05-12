9bc844f2d992b6efc83c8f7f6656b1213daf2647
Lei Zhou (周磊) <zhoulei@le.com>
2017-05-12 10:00:02 +0800

    keep modified file license

--- BlockCanaryExPlugin/src/main/groovy/com/letv/sarrsdesktop/blockcanaryex/SamplerInjecter.groovy
+++ BlockCanaryExPlugin/src/main/groovy/com/letv/sarrsdesktop/blockcanaryex/SamplerInjecter.groovy
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2017 lqcandqq13 (https://github.com/lqcandqq13).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.letv.sarrsdesktop.blockcanaryex
 
 import javassist.*
@@ -24,6 +9,11 @@ import java.util.jar.JarFile
 import java.util.jar.JarOutputStream
 import java.util.zip.ZipEntry
 
+/**
+ * modified from https://github.com/jasonross/NuwaGradle
+ *
+ * author: jixin.jia zhoulei date: 2017/3/11.
+ */
 class SamplerInjecter {
     private static ClassPool classPool;
 
